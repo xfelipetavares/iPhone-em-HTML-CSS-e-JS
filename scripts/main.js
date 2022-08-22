d9174b20c4cb843b7b09411a4cef9b1f2e465164
Felipe Tavares <xfelipetavares@gmail.com>
2022-08-22 17:27:49 -0300

    dialer

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+function nada(){
+    alert(`Nada aqui ainda, curioso(a) 👀`)
+}
+
+function goHome(){
+    document.getElementById(`iphone`).classList.remove('dialer');
+}
+function dialer(){
+    document.getElementById(`iphone`).classList.add('dialer');
+}
