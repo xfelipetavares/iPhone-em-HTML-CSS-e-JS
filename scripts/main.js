87452a0a7929bbf9b1dd6c4bdee8d3846444e2ff
Felipe Tavares <xfelipetavares@gmail.com>
2022-08-23 07:25:34 -0300

    ADICIONADO RELOGIO

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+
+
 function nada(){
     alert(`Nada aqui ainda, curioso(a) 👀`)
 }
@@ -7,4 +9,17 @@ function goHome(){
 }
 function dialer(){
     document.getElementById(`iphone`).classList.add('dialer');
+}
+
+
+function time(){
+    var clock_phone = document.getElementById(`clock-phone`);
+    var now = new Date();
+    var horas = now.getHours();
+    var minutos = now.getMinutes();
+    clock_phone.innerHTML = `${horas}:${minutos}`
+
+    requestAnimationFrame(time)
+    
+
 }
